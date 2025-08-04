917d867aafe3be40b1046cb9b6bca4d68b5a8549
ThiagoMTF <thiagomtf98@gmail.com>
2025-08-04 19:43:14 -0300

    banco config

--- IS.Store.Data/EF/DbInitializer.cs
+++ IS.Store.Data/EF/DbInitializer.cs
@@ -9,32 +9,12 @@ namespace IS.Store.Data.EF
     {
         protected override void Seed(ISStoreDataContextEF context)
         {
-            var alimento = new TipoDeProduto { Nome = "Alimento" };
-            var informatica = new TipoDeProduto { Nome = "Informática" };
-            var eletronico = new TipoDeProduto { Nome = "Eletrônico" };
-            var higiene = new TipoDeProduto { Nome = "Higiene" };
-            var limpeza = new TipoDeProduto { Nome = "Limpeza" };
-
-            var produtos = new List<Produto>() {
-                new Produto() {Nome="Picanha", Preco=79.99m, Qtde=150, TipoDeProduto = alimento },
-                new Produto() {Nome="Fraldinha", Preco=59.99m, Qtde=150, TipoDeProduto = alimento},
-                new Produto() {Nome="Pasta de Dente", Preco=9.99m, Qtde=200, TipoDeProduto = higiene},
-                new Produto() {Nome="Desinfetante", Preco=19.99m, Qtde=250, TipoDeProduto = limpeza},
-                new Produto() {Nome="Sabão em pó", Preco=20.99m, Qtde=250, TipoDeProduto = limpeza},
-                new Produto() {Nome="Sabonete", Preco=1.99m, Qtde=300, TipoDeProduto = higiene},
-                new Produto() {Nome="Computador", Preco=7999.99m, Qtde=50, TipoDeProduto = informatica},
-                new Produto() {Nome="Telefone", Preco=35.15m, Qtde=50, TipoDeProduto = eletronico}
-            };
-            context.Produtos.AddRange(produtos);
-
             context.Usuarios.Add(new Usuario()
             {
                 Nome = "Thiago Muniz",
                 Email = "thiagomuniz_infsys@outlook.com",
                 Senha = "123456".Encrypt()
             });
-
-
             context.SaveChanges();
         }
     }
